bd89dbe1d09505f3ccf3a54ae1e539973cfacde5
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-28 21:12:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,6 +52,21 @@ class Student
    end
   end
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+     SELECT id, name, grade
+     FROM students
+     WHERE grade = 10
+     LIMIT 10
+     SQL
+
+   found = DB[:conn].execute(sql)
+
+   found.map do |student|
+     self.new_from_db(student)
+   end
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT id, name, grade
